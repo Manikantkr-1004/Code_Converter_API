1a099eb166da2cd10b9d59deefea78d2d9442f49
Manikant Kumar <123896779+Manikantkr-1004@users.noreply.github.com>
2023-10-26 21:10:04 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const PORT = process.env.PORT || 7700;
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: 'https://codeconverterbymani.netlify.app',
+}));
 app.use(express.json());
 
 app.get("/",(req,res)=>{
@@ -66,7 +68,7 @@ app.post("/debug",async(req,res)=>{
 app.post("/quality",async(req,res)=>{
     const {code} = req.body;
 
-    let contents = `Please provide a code quality assessment for the given code. Consider the following parameters:\n1. Code Consistency: Evaluate the code for consistent coding style, naming conventions, and formatting.\n2. Code Performance: Assess the code for efficient algorithms, optimized data structures, and overall performance considerations.\n3. Code Documentation: Review the code for appropriate comments, inline documentation, and clear explanations of complex logic.\n4. Error Handling: Examine the code for proper error handling and graceful error recovery mechanisms.\n5. Code Testability: Evaluate the code for ease of unit testing, mocking, and overall testability.\n6. Code Modularity: Assess the code for modular design, separation of concerns, and reusability of components.\n7. Code Complexity: Analyze the code for excessive complexity, convoluted logic, and potential code smells.\n8. Code Duplication: Identify any code duplication and assess its impact on maintainability and readability.\n9. Code Readability: Evaluate the code for readability, clarity, and adherence to coding best practices.\nPlease provide a summary of the code quality assessment and a report showing the percentage-wise evaluation for each parameter mentioned above.\n Remember you have to give response in markdown language. \n here is my code \n ${code}`;
+    let contents = `Please provide a code quality assessment for the given code. Consider the following parameters:\n1. Code Consistency: Evaluate the code for consistent coding style, naming conventions, and formatting.\n2. Code Performance: Assess the code for efficient algorithms, optimized data structures, and overall performance considerations.\n3. Error Handling: Examine the code for proper error handling and graceful error recovery mechanisms.\n4. Code Testability: Evaluate the code for ease of unit testing, mocking, and overall testability.\n5. Code Complexity: Analyze the code for excessive complexity, convoluted logic, and potential code smells.\n6. Code Duplication: Identify any code duplication and assess its impact on maintainability and readability.\n7. Code Readability: Evaluate the code for readability, clarity, and adherence to coding best practices.\nPlease provide a summary of the code quality assessment and a report showing the percentage-wise evaluation for each parameter mentioned above.\n Remember you have to give response in markdown language. \n here is my code \n ${code}`;
 
     axios.post(process.env.APIURL,{
         model: process.env.APIMODEL,
